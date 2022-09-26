0fb62e4add024cbbc10d81c0a6667644966f1412
CyberBet4 <triumphanazia@gmail.com>
2022-09-26 10:05:19 +0100

    Register Controller updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,13 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use App\Http\Controllers\Controller;
-// use App\Providers\RouteServiceProvider;
 use App\Models\User;
-// use App\Models\socialProvider;
-// use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Facades\Validator;
+
 
 
 class RegisterController extends Controller
